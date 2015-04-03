e3b05985dbbd324338baa3414e09fcd23ca97da0
Nick Lu <Nikraaaazy@me.com>
2015-04-04 00:43:20 +0800

    Project Initialized with Class Word

---
+++ word.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class word
+{
+private:
+	string wordName; // word name
+	string wordPron; // word pronounciation
+	// make these three a struct ? 
+	string wordPS; // word part of speech
+	string trans; // word translation
+	string sentence; // word sentence
+
+	int wordLevel; // raw grey or done
+public:
+	word();
+	~word();
+	
+};
